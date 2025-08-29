0edc946a8c31942e6ea8cd821b83f0cec2dbb6f8
Adoniev Ivan <adonjew.vaniya@gmail.com>
2025-08-29 18:47:33 +0300

    fix: fixed page params

--- src/app/repository/[id]/page.tsx
+++ src/app/repository/[id]/page.tsx
@@ -1,19 +1,19 @@
 import getGHRepoById from '@/lib/api/github/requests/getGHRepoById';
-import { FC } from 'react';
 import { notFound } from 'next/navigation';
 import RepositoryFullPage from '@/app/repository/[id]/components/RepositoryFullPage/RepositoryFullPage';
 
 interface IProps {
-  params: {
+  params: Promise<{
     id: number;
-  };
+  }>;
 }
-const Page: FC<IProps> = async (props) => {
-  const repoId = props.params.id;
+const Page = async (props: IProps) => {
+  const repoId = (await props.params).id;
   try {
     const repository = await getGHRepoById({ id: repoId });
     return <RepositoryFullPage repository={repository} />;
   } catch (e) {
+    console.log(e);
     return notFound();
   }
 };
--- src/lib/hooks/useDebounceEffect.ts
+++ src/lib/hooks/useDebounceEffect.ts
@@ -3,7 +3,8 @@ import { DependencyList, useEffect } from 'react';
 export function useDebounceEffect(fn: () => void, waitTime: number, deps?: DependencyList) {
   useEffect(() => {
     const t = setTimeout(() => {
-      fn.apply(undefined);
+      // @ts-expect-error
+      fn.apply(undefined, deps);
     }, waitTime);
 
     return () => {
